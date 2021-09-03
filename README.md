04a1f217c0974fabd6df859d92e159ce5412372a
waffleonthestreet <woojeongkim610@gmail.com>
2021-09-03 13:16:46 +0900

    모두삭제

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # QandA
 Q&amp;A website (.NET Core, React)
+
+21.09.03 모두 제거 후 재시작
--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,8 @@
-using DbUp;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using QandA.Data;
-using QandA.Authorization;
-using Microsoft.AspNetCore.Authorization;
 
 namespace QandA
 {
@@ -24,43 +19,10 @@ namespace QandA
         public void ConfigureServices(IServiceCollection services)
         {
             var connectionString = Configuration.GetConnectionString("DefaultConnection");
-            EnsureDatabase.For.SqlDatabase(connectionString);
-            // TODO - Create and configure an instance of the DbUp upgrader
-            var upgrader = DeployChanges.To.SqlDatabase(connectionString, null).WithScriptsEmbeddedInAssembly(System.Reflection.Assembly.GetExecutingAssembly())
-                .WithTransaction()
-                .Build();
-            if (upgrader.IsUpgradeRequired())
-            {
-                upgrader.PerformUpgrade();
-            }
-
-            // TODO - Do a database migration if there are any pending SQL
-            //   pending SQL
-            // Scripts
-            services.AddScoped<IDataRepository, DataRepository>();
 
             services.AddRazorPages();
 
             services.AddControllersWithViews();
-
-            services.AddMemoryCache();
-            services.AddSingleton<IQuestionCache, QuestionCache>();
-
-            services.AddAuthentication(options => {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options => {
-                options.Authority = Configuration["Auth0:Authority"];
-                options.Audience = Configuration["Auth0:Audience"];
-            });
-
-            services.AddHttpClient();
-            services.AddAuthorization(options => options.AddPolicy("MustBeQuestionAuthor", policy => policy.Requirements.Add(new MustBeQuestionAuthorRequirement())));
-            services.AddScoped<IAuthorizationHandler, MustBeQuestionAuthorHandler>();
-            services.AddHttpContextAccessor();
-
-            // CORS
-            services.AddCors(options => options.AddPolicy("CorsPolicy", builder => builder.AllowAnyMethod().AllowAnyHeader().WithOrigins(Configuration["Frontend"])));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -81,10 +43,6 @@ namespace QandA
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseCors("CorsPolicy");
-
-            app.UseAuthentication();
 
             app.UseAuthorization();
 
